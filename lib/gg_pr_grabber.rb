73ff4a2dc0716fa44274fb48e9d44f24c0e1d722
kevhsu <kev@kvhsu.com>
2016-07-08 03:58:10 -0700

    smashgg rankings scraper complete

--- lib/gg_pr_grabber.rb
+++ lib/gg_pr_grabber.rb
@@ -11,6 +11,10 @@ class GgPrGrabber
     @http = HTTPClient.new
   end
 
+  def method_name
+    
+  end
+
   def get_all_ranking_urls
     index = 1
     ret_val = []
@@ -23,9 +27,44 @@ class GgPrGrabber
   end
 
   def get_ranking_urls(page_num)
+    puts "rankings page #{page_num}"
     body = @http.get(RANKING_INDEX_URL + page_num.to_s).body
     doc = Nokogiri::HTML.parse(body)
 
-    ranking_pages = doc.css("div.gg-card/a").map{|card| BASE_URL + card['href']}
+    ranking_pages ||= doc.css("div.gg-card/a").map{|card| BASE_URL + card['href']}
+  end
+
+  def player_region_map
+    ret_val = {}
+    pr_file = File.expand_path('../../tmp/gg_pr.json', __FILE__)
+    if File.exists?(pr_file)
+      ret_val = JSON.parse(File.read(pr_file))
+    else
+      get_all_ranking_urls.each do |url|
+        puts "parsing #{url}"
+        ret_val.merge!(parse_ranking_page(url))
+      end
+      File.open(pr_file, "w") do |f|
+        f.write(ret_val.to_json)
+      end
+    end
+    ret_val
+  end
+
+  def parse_ranking_page(ranking_url)
+    body = @http.get(ranking_url).body
+    doc = Nokogiri::HTML.parse(body)
+    ret_val = {}
+
+    ranking_name = doc.at_css('h3.heading-no-margin-bottom').content
+    players = doc.css('div.gamertag-title').map{|player| player.content}
+    players.each do |player|
+      ret_val[player] = ranking_name
+    end
+    ret_val
   end
 end
+
+g = GgPrGrabber.new
+#puts g.get_all_ranking_urls
+g.player_region_map
